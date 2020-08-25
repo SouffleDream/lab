5175fff3197a59005d40392145f1fc04c0307e36
SouffleDream <62616968+SouffleDream@users.noreply.github.com>
2020-08-26 00:45:18 +0200

    Add files via upload

--- tps/TP2_Elise_LLEDO.py
+++ tps/TP2_Elise_LLEDO.py
@@ -1,45 +1,78 @@
 #!/usr/bin/env python
 
-from argparse import ArgumentParser
+import argparse
 import threading
 import os
-from time import time
-import args as args
+import time
 
 barrier = threading.Barrier(3)
 lock = threading.Lock()
 
-def argument():
-
+def parse_args():
     # Make the argument prog list
-    parser = ArgumentParser(description='Steganography')
+    parser = argparse.ArgumentParser(description="Steganography")
 
-    parser.add_argument("-s", "--size", type=int, default=1024, help="Reading block")
-    parser.add_argument("-f", "--file", default="dog.ppm", help="PPM file, Image to analized")
-    parser.add_argument("-m", "--message", default="message.txt", help="Steganographic message")
-    parser.add_argument("-of", "--offset", type=int, default=15, help="Offset in pixels of raster start")
-    parser.add_argument("-i", "--interleave", type=int, default=10, help="pixel mod interleave")
-    parser.add_argument("-o", "--output", default="output_message.ppm", help="Stego-message")
+    parser.add_argument(
+        "-s",
+        "--size",
+        type=check_positive,
+        default=1,
+        help="Buffer size"
+    )
+    parser.add_argument(
+        "-f",
+        "--file",
+        help="PPM file, Image to analyze",
+        #required=True,
+        default="dog.ppm",
+        type=check_path,
+    )
+    parser.add_argument(
+        "-m",
+        "--message",
+        #required=True,
+        default="message.txt",
+        type=check_path,
+        help="Text file wich contain message to put in image",
+    )
+    parser.add_argument(
+        "-of",
+        "--offset",
+        type=check_positive,
+        default=15,
+        help="Offset in pixels of raster start",
+    )
+    parser.add_argument(
+        "-i",
+        "--interleave",
+        type=check_positive,
+        default=10,
+        help="pixel mod interleave",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="Result path of file",
+        default="output_message.ppm"
+    )
 
     return parser.parse_args()
 
-def verify_error(args):
-    # Verify there is no problem
-    if args.offset <= 0 or args.interleave <= 0 or args.size <= 0:
-        print("\n***ERROR***")
-        print("Values have to be higher than zero")
-        exit(-1)
+
+# Verify if the file have no error before beginning
+def check_positive(value):
+    value = int(value)
+    if value < 0:
+        raise argparse.ArgumentTypeError(
+            "{} is an invalid positive int value".format(value)
+        )
+    return value
 
 
-    try:
-        file = os.open(args.file, os.O_RDONLY)  # open the ppm image
-        message = os.open(args.message, os.O_RDONLY)    #open the steganographic message
-    except FileNotFoundError:
-        print("\n***ERROR***")
-        print("Opening file failed")
-        exit(-1)
-
-    return [file, message]
+def check_path(path):
+    if os.path.exists(path):
+        return path
+    raise argparse.ArgumentTypeError("{} is not found".format(path))
 
 
 def offset(read):
@@ -56,23 +89,15 @@ def offset(read):
 
     return deep + 1
 
+
 def read_Message(message, size):
-    mes = os.open(message, os.O_RDONLY)
-    mes_read = os.read(mes, size)
-    mesage = []
-    for i in mes_read:
-        mesage.append("{0:b}".format(i))
-    count = 0
-    for caract in mesage:
-        for j in range(8-(len(caract))):
-            charact = "0" + caract
-        mesage[count] = charact
-        count += 1
-    mes_bin = ""
-    for k in mesage:
-        mes_bin += k
-    l_total = len(mes_bin)
-    return mes_bin, l_total
+    with open(message, "rb") as f:
+        content = list()
+        buffer = f.read(size)
+        while buffer:
+            content.append(bin(int.from_bytes(buffer, byteorder="big")))
+            buffer = f.read(size)
+        return content, len(content)
 
 
 def steganography(message, pixels, begin_message, indice_pixel, interleave):
@@ -85,10 +110,12 @@ def steganography(message, pixels, begin_message, indice_pixel, interleave):
             break
 
         # transform en binary the pixel
-        bin="{0:b}".format(pixels[i]) #string
-        bin = bin[0:-1]+(message[indice])
-        pixels[i] = int(bin, 2)
-    barrier.wait()
+        bin=format(pixels[i], "#b") #string
+        if bin[-1] != bin[indice]:
+            bin = bin[:-1]+(message[indice])
+            pixels[i] = int(bin, 2)
+        indice += 3
+    #barrier.wait()
     return pixels
 
 
@@ -107,26 +134,24 @@ def outputMessage(red, blue, green, output, header):
         print("Opening file failed")
         exit(-1)
     pixels.append(header, output)
-    barrier.wait()
 
 
 
 if __name__ == '__main__':
-    args = argument()  # Begin with the definition of the argument list
-    start_time = time()
-    [file, message] = verify_error(args)  # Verify if the file have no error before beginning
-    #message = message.encode('ascii')
+    args = parse_args()  # Begin with the definition of the argument list
+    start_time = time.time()
     message, l_total = read_Message(args.message, args.size) #Read the message to encrypt
 
     ##Definition of header
     size_header = 100  # definition of size max for header
 
-    read = os.read(file, size_header)  # read the file
+    doc = os.open(args.file, os.O_RDONLY)
+    read = os.read(doc, size_header)  # read the file
     offset = offset(read)  # calcul offset
     header = read[:offset]  # read the information of ppm file (number of cols and lines, and the encoding)
     comment = "#UMCOMPU2 {} {} {}\n".format(args.offset, args.interleave, l_total)
     header = (read[0:2], "\n", comment, read[2:])
-    os.lseek(file, offset, 0)  # Position the read / write head in the file
+    os.lseek(doc, offset, 0)  # Position the read / write head in the file
 
     output = open(args.output, "wb", os.O_CREAT)
 
@@ -140,13 +165,20 @@ if __name__ == '__main__':
     indice_green = 1
     indice_blue = 2
 
+
     # Define the width of the message (end when the entire message is encrypted)
-    while True:
+    '''while True:
         lock.acquire()
         #mes = message.read(l_total)
         pixels = [i for i in message]
         lock.release()
-        barrier.wait()
+        barrier.wait()'''
+    lock.acquire()
+    # mes = message.read(l_total)
+    pixels = [i for i in message]
+    lock.release()
+    #barrier.wait()
+
 
     # Creation of 3 thread, one for each color
     thread_red = threading.Thread(target=steganography, args=(message, pixels, begin_red, indice_red, args.interleave))
@@ -166,9 +198,10 @@ if __name__ == '__main__':
     outputMessage(red, blue, green, args.output, header)
 
     if indice_red >= len(message) and indice_green >= len(message) and indice_blue >= len(message):
-        print("The output_message have been generated.\n Duration: ", (time()-start_time))
+        print("The output_message have been generated.\n Duration: ", format(time.time()-start_time))
         exit(-1)
 
     message.close()
     args.file.close()
     args.output.close()
+
